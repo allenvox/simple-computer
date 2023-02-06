3c0b0076576ca029b1a085ebeae77de4896076c4
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-06 21:51:51 +0700

    Fix code format

--- src/msc.h
+++ src/msc.h
@@ -1,33 +1,25 @@
 #pragma once
 
-int
-sc_memoryInit (); // initializes the array of 100 elements
+int sc_memoryInit (); // initializes the array of 100 elements
 
-int
-sc_memorySet (int address,
+int sc_memorySet (int address,
                   int value); // sets the value of [address] memory unit
 
-int
-sc_memoryGet (int address, // gets the value of [address] memory unit and
+int sc_memoryGet (int address, // gets the value of [address] memory unit and
                   int *value); // returns it into value var
 
-int
-sc_memorySave (
+int sc_memorySave (
     char *filename); // saves memory into a binary file (write/fwrite)
 
-int
-sc_memoryLoad (char *filename); // loads RAM from a file (read/fread)
+int sc_memoryLoad (char *filename); // loads RAM from a file (read/fread)
 
-int
-sc_regInit (void); // inits the register of flags with 0
+int sc_regInit (void); // inits the register of flags with 0
 
-int
-sc_regSet (
+int sc_regSet (
     int register, // sets the flag register value, #define-s are used for
     int value);   // register numbers, if wrong register number - error
 
-int
-sc_regGet (int register,
+int sc_regGet (int register,
                int *value); // gets the flag value, if wrong register - error
 
 int
