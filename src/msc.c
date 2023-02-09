22cdb83dbf99db999abb95e2d300e0deac57640e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-09 09:44:07 +0700

    Add spaces before sizeof()

--- src/msc.c
+++ src/msc.c
@@ -9,7 +9,7 @@ int sc_register;
 int
 sc_memoryInit () // initializes the array of 100 elements
 {
-  memset (sc_memory, 0, MEMSIZE * sizeof(sc_memory[0]));
+  memset (sc_memory, 0, MEMSIZE * sizeof (sc_memory[0]));
   return 0;
 }
 
@@ -48,7 +48,7 @@ sc_memorySave (
     {
       return 1;
     }
-  fwrite (sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
+  fwrite (sc_memory, sizeof (int), sizeof (sc_memory) / sizeof (int), f);
   fclose (f);
   return 0;
 }
@@ -61,7 +61,7 @@ sc_memoryLoad (char *filename) // loads RAM from a file (read/fread)
     {
       return 1;
     }
-  fread (sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
+  fread (sc_memory, sizeof (int), sizeof (sc_memory) / sizeof (int), f);
   fclose (f);
   return 0;
 }
