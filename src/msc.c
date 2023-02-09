a362c76774e4bd427c6dfb0624475c97c5cbd2ea
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-09 09:41:13 +0700

    Format code

--- src/msc.c
+++ src/msc.c
@@ -18,10 +18,10 @@ sc_memorySet (int address,
               int value) // sets the value of [address] memory unit
 {
   if (address < 0 || address >= MEMSIZE)
-  {
-    BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
-    return FLAG_WRONG_ADDRESS;
-  }
+    {
+      BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
+      return FLAG_WRONG_ADDRESS;
+    }
   sc_memory[address] = value;
   return 0;
 }
@@ -31,10 +31,10 @@ sc_memoryGet (int address, // gets the value of [address] memory unit and
               int *value)  // returns it into value var
 {
   if (address < 0 || address >= MEMSIZE)
-  {
-    BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
-    return FLAG_WRONG_ADDRESS;
-  }
+    {
+      BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
+      return FLAG_WRONG_ADDRESS;
+    }
   *value = sc_memory[address];
   return 0;
 }
@@ -45,9 +45,9 @@ sc_memorySave (
 {
   FILE *f = fopen(filename, "wb");
   if (!f)
-   {
-     return 1;
-   }
+    {
+      return 1;
+    }
   fwrite(sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
   fclose(f);
   return 0;
@@ -58,9 +58,9 @@ sc_memoryLoad (char *filename) // loads RAM from a file (read/fread)
 {
   FILE *f = fopen(filename, "rb");
   if (!f)
-   {
-     return 1;
-   }
+    {
+      return 1;
+    }
   fread(sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
   fclose(f);
   return 0;
@@ -78,19 +78,19 @@ sc_regSet (int reg,   // sets the flag register value, #define-s are used for
            int value) // register numbers, if wrong register number - error
 {
   if (reg < 0 || reg > 4)
-   {
-     return ERR_WRONG_FLAG;
-   }
+    {
+      return ERR_WRONG_FLAG;
+    }
   if (!value)
-   {
-     BIT_DEL(sc_register, reg);
-     return 0;
-   }
+    {
+      BIT_DEL(sc_register, reg);
+      return 0;
+    }
   if (value != 1)
-   {
-     BIT_SET(sc_register, FLAG_OVERFLOW);
-     return ERR_WRONG_VALUE;
-   }
+    {
+      BIT_SET(sc_register, FLAG_OVERFLOW);
+      return ERR_WRONG_VALUE;
+    }
   BIT_SET(sc_register, reg);
   return 0;
 }
@@ -100,9 +100,9 @@ sc_regGet (int reg,
            int *value) // gets the flag value, if wrong register - error
 {
   if (reg < 0 || reg > 4)
-   {
-     return ERR_WRONG_FLAG;
-   }
+    {
+      return ERR_WRONG_FLAG;
+    }
   *value = BIT_GET(sc_register, reg);
   return 0;
 }
