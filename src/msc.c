5aacedbb6f0470a3038d150c65f750fc13324d55
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-09 09:36:58 +0700

    Format code

--- src/msc.c
+++ src/msc.c
@@ -1,7 +1,7 @@
 #include "msc.h"
 #include <inttypes.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int sc_memory[MEMSIZE];
 int sc_register;
@@ -9,102 +9,102 @@ int sc_register;
 int
 sc_memoryInit () // initializes the array of 100 elements
 {
-    memset(sc_memory, 0, MEMSIZE * sizeof(sc_memory[0]));
-    return 0;
+  memset(sc_memory, 0, MEMSIZE * sizeof(sc_memory[0]));
+  return 0;
 }
 
 int
 sc_memorySet (int address,
               int value) // sets the value of [address] memory unit
 {
-    if (address < 0 || address >= MEMSIZE)
-    {
-        BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
-        return FLAG_WRONG_ADDRESS;
-    }
-    sc_memory[address] = value;
-    return 0;
+  if (address < 0 || address >= MEMSIZE)
+  {
+    BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
+    return FLAG_WRONG_ADDRESS;
+  }
+  sc_memory[address] = value;
+  return 0;
 }
 
 int
 sc_memoryGet (int address, // gets the value of [address] memory unit and
               int *value)  // returns it into value var
 {
-    if (address < 0 || address >= MEMSIZE)
-    {
-        BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
-        return FLAG_WRONG_ADDRESS;
-    }
-    *value = sc_memory[address];
-    return 0;
+  if (address < 0 || address >= MEMSIZE)
+  {
+    BIT_SET(sc_register, FLAG_WRONG_ADDRESS);
+    return FLAG_WRONG_ADDRESS;
+  }
+  *value = sc_memory[address];
+  return 0;
 }
 
 int
 sc_memorySave (
     char *filename) // saves memory into a binary file (write/fwrite)
 {
-    FILE *f = fopen(filename, "wb");
-    if (!f)
-    {
-        return 1;
-    }
-    fwrite(sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
-    fclose(f);
-    return 0;
+  FILE *f = fopen(filename, "wb");
+  if (!f)
+  {
+    return 1;
+  }
+  fwrite(sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
+  fclose(f);
+  return 0;
 }
 
 int
 sc_memoryLoad (char *filename) // loads RAM from a file (read/fread)
 {
-    FILE *f = fopen(filename, "rb");
-    if (!f)
-    {
-        return 1;
-    }
-    fread(sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
-    fclose(f);
-    return 0;
+  FILE *f = fopen(filename, "rb");
+  if (!f)
+  {
+    return 1;
+  }
+  fread(sc_memory, sizeof(int), sizeof(sc_memory) / sizeof(int), f);
+  fclose(f);
+  return 0;
 }
 
 int
 sc_regInit (void) // inits the register of flags with 0
 {
-    sc_register = 0;
-    return 0;
+  sc_register = 0;
+  return 0;
 }
 
 int
 sc_regSet (int reg,   // sets the flag register value, #define-s are used for
            int value) // register numbers, if wrong register number - error
 {
-    if (reg < 0 || reg > 4)
-    {
-        return ERR_WRONG_FLAG;
-    }
-    if (!value)
-    {
-        BIT_DEL(sc_register, reg);
-        return 0;
-    }
-    if (value != 1)
-    {
-        BIT_SET(sc_register, FLAG_OVERFLOW);
-        return ERR_WRONG_VALUE;
-    }
-    BIT_SET(sc_register, reg);
+  if (reg < 0 || reg > 4)
+  {
+    return ERR_WRONG_FLAG;
+  }
+  if (!value)
+  {
+    BIT_DEL(sc_register, reg);
     return 0;
+  }
+  if (value != 1)
+  {
+    BIT_SET(sc_register, FLAG_OVERFLOW);
+    return ERR_WRONG_VALUE;
+  }
+  BIT_SET(sc_register, reg);
+  return 0;
 }
 
 int
 sc_regGet (int reg,
            int *value) // gets the flag value, if wrong register - error
 {
-    if (reg < 0 || reg > 4)
-    {
-        return ERR_WRONG_FLAG;
-    }
-    *value = BIT_GET(sc_register, reg);
-    return 0;
+  if (reg < 0 || reg > 4)
+  {
+    return ERR_WRONG_FLAG;
+  }
+  *value = BIT_GET(sc_register, reg);
+  return 0;
 }
 
 int
