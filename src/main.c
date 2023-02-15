d429e5aed406fd3d8492834e6ac29b0764cf1af4
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-15 11:03:55 +0700

    Implement testing module for term.h module

--- src/main.c
+++ src/main.c
@@ -4,6 +4,21 @@
 int
 main ()
 {
-  
+  mt_clrscr ();
+  printf ("hello, this is default terminal\n");
+  getchar ();
+
+  mt_clrscr ();
+  mt_setbgcolor (GREEN);
+  printf ("hello, this is green background for outputted text\n");
+  getchar ();
+
+  mt_setbgcolor (DEFAULT);
+  mt_clrscr ();
+  mt_setfgcolor (RED);
+  printf ("hello, this is red background for everything\n");
+  getchar ();
+
+  mt_clrscr ();
   return 0;
 }
