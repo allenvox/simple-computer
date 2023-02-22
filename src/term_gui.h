0e648285a7138306784f3689d4def9c4b82e42a6
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-22 12:38:06 +0700

    Add new function prototypes to GUI header file

--- src/term_gui.h
+++ src/term_gui.h
@@ -1,9 +1,20 @@
 #pragma once
 
-#include "msc.h"
+#include <sys/param.h>
 
-void out_N_hor (int n);
-void out_border_top ();
-void out_flags ();
-void out_memcell (int n);
-void out_GUI ();
+#define BC_X 15
+#define BC_START 8
+#define BC_STEP 8
+#define X_START 2
+#define Y_START 3
+#define Y_STEP 6
+
+void g_memorybox ();
+void g_accumbox ();
+void g_counterbox ();
+void g_operationbox ();
+void g_flagbox ();
+void g_bcbox (int *big);
+char *g_flags ();
+void g_static ();
+void g_printbc (int x, int y);
