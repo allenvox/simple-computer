73224d9a204a32d34fc29d2872681f2753a3463b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-23 12:19:27 +0700

    Implement bigchar array with bcbox outputting

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "bc.h"
 #include "msc.h"
 #include "term.h"
 #include "term_gui.h"
@@ -8,11 +9,29 @@
 int
 main ()
 {
+  int big[18][2] = {{0x4242423C, 0x3C424242},  // 0
+                      {0x48506040, 0x40404040},  // 1
+                      {0x2042423C, 0x7E020418},  // 2
+                      {0x3840423C, 0x3C424040},  // 3
+                      {0x7E424478, 0x40404040},  // 4
+                      {0x3E02027E, 0x3C424040},  // 5
+                      {0x3E02423C, 0x3C424242},  // 6
+                      {0x2040407E, 0x10101010},  // 7
+                      {0x3C42423C, 0x3C424242},  // 8
+                      {0x7C42423C, 0x3C424040},  // 9
+                      {0x66663C18, 0x66667E7E},  // A
+                      {0x3E66663E, 0x3E666666},  // B
+                      {0x0202423C, 0x3C420202},  // C
+                      {0x4444443E, 0x3E444444},  // D
+                      {0x3E02027E, 0x7E020202},  // E
+                      {0x1E02027E, 0x02020202},  // F
+                      {0x7E181800, 0x0018187E},  // +
+                      {0x00181800, 0x00181800}}; // :
   mt_clrscr ();
   sc_memoryInit ();
-  for (int i = 3; i < 100; i *= 3)
+  for (int i = 3; i < 100; i += 3)
     {
-      sc_memorySet (i, i);
+      sc_memorySet (i, i*3);
     }
   sc_regInit ();
   sc_regSet (1, 1);
@@ -22,11 +41,11 @@ main ()
   sc_accumSet(312);
   sc_countSet(11);
   g_static ();
-  int big[36];
   g_memorybox ();
   g_accumbox ();
   g_counterbox ();
   g_operationbox ();
   g_flagbox ();
+  g_bcbox (big);
   return 0;
 }
