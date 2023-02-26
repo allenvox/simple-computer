e182f8d073d63a2b690f61f7edc205bc72f4bcb4
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-02-26 11:47:44 +0700

    Add header file for readkey module

---
+++ src/readkey.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <termios.h>
+
+struct termios current, backup;
+
+enum keys
+{
+  KEY_S,
+  KEY_L,
+  KEY_R,
+  KEY_T,
+  KEY_I,
+  KEY_F5,
+  KEY_F6 ,
+  KEY_UP,
+  KEY_DOWN,
+  KEY_RIGHT,
+  KEY_LEFT,
+  KEY_ENTER,
+  KEY_ESCAPE,
+  KEY_DEFAULT,
+};
+
+int rk_readkey (enum keys *key);
+int rk_mytermsave (void);
+int rk_mytermrestore (void);
+int rk_mytermregime (int regime, int vtime, int vmin, int echo, int sigint);
