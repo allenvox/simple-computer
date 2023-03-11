b580a42071f4946fea1715a65214dae900ddee3a
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-03-11 12:36:50 +0700

    Add signals handling to main function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "bc.h"
 #include "msc.h"
 #include "readkey.h"
+#include "sig.h"
 #include "term.h"
 #include "tui.h"
 #include <stdio.h>
@@ -10,20 +11,20 @@
 int
 main ()
 {
-  mt_clrscr ();
-  rk_mytermregime (0, 0, 1, 0, 1);
   sc_memoryInit ();
   for (int i = 3; i < 100; i += 3)
     {
       sc_memorySet (i, i * 3);
     }
   sc_regInit ();
-  sc_regSet (1, 1);
-  sc_regSet (2, 1);
-  sc_regSet (4, 1);
-  sc_regSet (16, 1);
+  sc_regSet (FLAG_IGNORE, 1);
   sc_accumSet (9999);
   sc_countSet (0);
+  sig_setSignals ();
+
+  rk_mytermsave ();
+  rk_mytermregime (0, 0, 1, 0, 1);
+  mt_clrscr ();
   g_interface ();
   rk_mytermrestore ();
   return 0;
