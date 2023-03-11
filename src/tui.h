a090caae098f8e838a37a9770f4ac751d7ace26e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-03-11 22:19:56 +0700

    Add libraries including & remove bigchars array dependency from header

--- src/tui.h
+++ src/tui.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include "bc.h"
+#include "msc.h"
+#include "readkey.h"
+#include "term.h"
+#include "sig.h"
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/param.h>
+#include <unistd.h>
 
 #define BC_X 15
 #define BC_START 8
@@ -14,11 +23,11 @@ int g_accumbox (void);
 int g_counterbox (void);
 int g_operationbox (void);
 int g_flagbox (void);
-int g_bcbox (int *big);
+int g_bcbox (void);
 int g_flags (char **val);
 int g_static (void);
 int g_loadmemory (void);
-int g_init (int *big);
+int g_init (void);
 int g_savememory (void);
 int g_printmem (int *x, int *y);
 int g_interface (void);
