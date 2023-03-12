f7e9022410854542663429c09e0900a265957593
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-03-12 15:11:44 +0700

    Rework functions for interface

--- src/tui.c
+++ src/tui.c
@@ -1,14 +1,7 @@
 #include "tui.h"
-#include "bc.h"
-#include "msc.h"
-#include "readkey.h"
-#include "term.h"
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
 
 int
-g_flags (char **val)
+g_writeflags (char **val)
 {
   int flag = 0;
   sc_regGet (1, &flag);
@@ -21,14 +14,16 @@ g_flags (char **val)
   char O = flag == 1 ? 'O' : ' ';
   sc_regGet (16, &flag);
   char C = flag == 1 ? 'C' : ' ';
-  char buff[14];
-  sprintf (buff, "%c  %c  %c  %c  %c", F, D, A, O, C);
+  sc_regGet (32, &flag);
+  char I = flag == 1 ? 'I' : ' ';
+  char buff[24] = {0};
+  sprintf (buff, "%c  %c  %c  %c  %c  %c\0", F, D, A, O, C, I);
   *val = buff;
   return 0;
 }
 
 int
-g_static (void)
+g_drawborders (void)
 {
   bc_box (1, 1, 12, 63);
   mt_gotoXY (0, 28);
@@ -66,26 +61,7 @@ g_static (void)
 }
 
 int
-g_memorybox (void)
-{
-  int k = 0;
-  for (int i = X_START; i < 12; i++)
-    {
-      for (int j = Y_START; j < 63; j += Y_STEP)
-        {
-          mt_gotoXY (i, j);
-          char buff[6];
-          int val;
-          sc_memoryGet (k++, &val);
-          sprintf (buff, "+%04d", val);
-          write (STDERR_FILENO, buff, 6 * sizeof (char));
-        }
-    }
-  return 0;
-}
-
-int
-g_accumbox (void)
+g_drawaccumbox (void)
 {
   mt_gotoXY (2, 80);
   char buff[5];
@@ -98,46 +74,111 @@ g_accumbox (void)
 }
 
 int
-g_counterbox (void)
+g_drawflagbox (void)
 {
-  mt_gotoXY (5, 80);
-  char buff[5];
-  int val;
-  sc_countGet (&val);
-  sprintf (buff, "%04d", val);
-  write (STDOUT_FILENO, buff, 5 * sizeof (char));
-  mt_gotoXY (33, 0);
-  return 0;
-}
-
-int
-g_operationbox (void)
-{
-  mt_gotoXY (8, 79);
-  write (STDOUT_FILENO, "+00 : 00", 8 * sizeof (char));
-  mt_gotoXY (33, 0);
-  return 0;
-}
-
-int
-g_flagbox (void)
-{
-  mt_gotoXY (11, 79);
+  mt_gotoXY (11, 78);
   char *val;
-  g_flags (&val);
-  write (STDOUT_FILENO, val, 14 * sizeof (char));
+  g_writeflags (&val);
+  write (STDOUT_FILENO, val, 24 * sizeof (char));
   mt_gotoXY (33, 0);
   return 0;
 }
 
 int
-g_bcbox (int *big)
+g_drawbcbox ()
 {
-  int count = 0;
-  int val = 0;
-  sc_countGet (&count);
-  sc_memoryGet (count, &val);
-  int digit[2] = { big[2 * 16], big[2 * 16 + 1] };
+  int big[18][2] = {
+    { 0x4242423C, 0x3C424242 }, // 0
+    { 0x48506040, 0x40404040 }, // 1
+    { 0x2042423C, 0x7E020418 }, // 2
+    { 0x3840423C, 0x3C424040 }, // 3
+    { 0x7E424478, 0x40404040 }, // 4
+    { 0x3E02027E, 0x3C424040 }, // 5
+    { 0x3E02423C, 0x3C424242 }, // 6
+    { 0x2040407E, 0x10101010 }, // 7
+    { 0x3C42423C, 0x3C424242 }, // 8
+    { 0x7C42423C, 0x3C424040 }, // 9
+    { 0x66663C18, 0x66667E7E }, // A
+    { 0x3E66663E, 0x3E666666 }, // B
+    { 0x0202423C, 0x3C420202 }, // C
+    { 0x4444443E, 0x3E444444 }, // D
+    { 0x3E02027E, 0x7E020202 }, // E
+    { 0x1E02027E, 0x02020202 }, // F
+    { 0x7E181800, 0x0018187E }, // +
+    { 0x7E000000, 0x7E }        // -
+  };
+  int address, val;
+  sc_countGet (&address);
+  char buff[6];
+  g_getunit (address, &buff);
+  int *digit = malloc (2 * sizeof (int));
+  for (int i = 0; i < 5; i++)
+    {
+      int k = 0;
+      switch (buff[i])
+        {
+        case '0':
+          k = 0;
+          break;
+        case '1':
+          k = 1;
+          break;
+        case '2':
+          k = 2;
+          break;
+        case '3':
+          k = 3;
+          break;
+        case '4':
+          k = 4;
+          break;
+        case '5':
+          k = 5;
+          break;
+        case '6':
+          k = 6;
+          break;
+        case '7':
+          k = 7;
+          break;
+        case '8':
+          k = 8;
+          break;
+        case '9':
+          k = 9;
+          break;
+        case 'A':
+          k = 10;
+          break;
+        case 'B':
+          k = 11;
+          break;
+        case 'C':
+          k = 12;
+          break;
+        case 'D':
+          k = 13;
+          break;
+        case 'E':
+          k = 14;
+          break;
+        case 'F':
+          k = 15;
+          break;
+        case '+':
+          k = 16;
+          break;
+        default:
+          k = 17;
+          break;
+        }
+      digit[0] = big[k][0];
+      digit[1] = big[k][1];
+      bc_printbigchar (digit, BC_X, BC_START + i * BC_STEP, GREEN, GREY);
+    }
+  /*
+  digit[0] = big[16][0];
+  digit[1] = big[16][1];
   bc_printbigchar (digit, BC_X, BC_START, GREEN, GREY);
   for (int i = 3; i >= 0; i--)
     {
@@ -197,9 +238,11 @@ g_bcbox (int *big)
           k = 17;
           break;
         }
-      int digit[2] = { big[2 * k], big[2 * k + 1] };
+      digit[0] = big[k][0];
+      digit[1] = big[k][1];
       bc_printbigchar (digit, BC_X, BC_START + (4 - i) * BC_STEP, GREEN, GREY);
     }
+  */
   mt_gotoXY (33, 0);
   return 0;
 }
@@ -240,7 +283,7 @@ g_loadmemory (void)
     }
   mt_setbgcolor (GREY);
   read (STDOUT_FILENO, NULL, 1);
-  g_static ();
+  g_drawboxes ();
   return 0;
 }
 
@@ -271,19 +314,7 @@ g_savememory (void)
   write (STDOUT_FILENO, " Success! \n", strlen (" Success! \n"));
   mt_setbgcolor (GREY);
   read (STDOUT_FILENO, NULL, 1);
-  g_static ();
-  return 0;
-}
-
-int
-g_init (int *big)
-{
-  g_memorybox ();
-  g_accumbox ();
-  g_counterbox ();
-  g_operationbox ();
-  g_flagbox ();
-  g_bcbox (big);
+  g_drawboxes ();
   return 0;
 }
 
@@ -310,32 +341,48 @@ g_setmemory (int x, int y)
   buff[4][0] = '\0';
   int val = atoi (buff);
   sc_memorySet (x * 10 + y, val);
-  g_static ();
-  g_memorybox ();
+  g_drawboxes ();
   return 0;
 }
 
 int
-g_printmem (int *x, int *y)
+g_getunit (int address, char **buff)
 {
-  g_memorybox ();
-  *x %= 10;
-  *y %= 10;
-  if (*x == -1)
-    {
-      *x = 9;
-    }
-  if (*y == -1)
-    {
-      *y = 9;
-    }
-  mt_gotoXY (X_START + (*x), Y_START + (*y) * Y_STEP);
-  char buff[6];
-  int address = (*x) * 10 + (*y);
-  sc_countSet (address);
-  int val;
+  int val, command, operand;
   sc_memoryGet (address, &val);
-  sprintf (buff, "+%04d", val);
+  char temp[6];
+  int err = sc_commandDecode (val, &command, &operand);
+  sprintf (temp, " %02x%02x", command, operand);
+  if (err == ERR_WRONG_COMMAND)
+    {
+      temp[0] = '-';
+    }
+  else if (err == 0)
+    {
+      temp[0] = '+';
+    }
+  strcpy (*buff, temp);
+  return 0;
+}
+
+int
+g_highlightmemory (int x, int y)
+{
+  g_drawmemorybox ();
+  x %= 10;
+  y %= 10;
+  if (x == -1)
+    {
+      x = 9;
+    }
+  if (y == -1)
+    {
+      y = 9;
+    }
+  int address = x * 10 + y;
+  char buff[6];
+  g_getunit (address, &buff);
+  mt_gotoXY (X_START + x, Y_START + y * Y_STEP);
   mt_setbgcolor (BLUE);
   write (STDERR_FILENO, buff, 6 * sizeof (char));
   mt_setbgcolor (GREY);
@@ -343,43 +390,82 @@ g_printmem (int *x, int *y)
 }
 
 int
+g_drawoperationbox (void)
+{
+  mt_gotoXY (8, 79);
+  int address;
+  sc_countGet (&address);
+  char buff[6];
+  g_getunit (address, &buff);
+  char result [7];
+  sprintf (result, "%c%c%c:%c%c", buff[0], buff[1], buff[2], buff[3], buff[4]);
+  write (STDOUT_FILENO, result, 7 * sizeof (char));
+  mt_gotoXY (33, 0);
+  return 0;
+}
+
+int
+g_drawcounterbox (void)
+{
+  mt_gotoXY (5, 80);
+  char buff[5];
+  int val;
+  sc_countGet (&val);
+  sprintf (buff, "%04d", val);
+  write (STDOUT_FILENO, buff, 5 * sizeof (char));
+  mt_gotoXY (33, 0);
+  return 0;
+}
+
+int
+g_drawmemorybox (void)
+{
+  int k = 0;
+  for (int i = X_START; i < 12; i++)
+    {
+      for (int j = Y_START; j < 63; j += Y_STEP)
+        {
+          mt_gotoXY (i, j);
+          char buff[6];
+          g_getunit (k++, &buff);
+          write (STDERR_FILENO, buff, 6 * sizeof (char));
+        }
+    }
+  return 0;
+}
+
+int
+g_drawboxes ()
+{
+  g_drawmemorybox ();
+  g_drawaccumbox ();
+  g_drawcounterbox ();
+  g_drawoperationbox ();
+  g_drawflagbox ();
+  g_drawbcbox ();
+  return 0;
+}
+
+int
 g_interface ()
 {
-  int big[18][2] = {
-    { 0x4242423C, 0x3C424242 }, // 0
-    { 0x48506040, 0x40404040 }, // 1
-    { 0x2042423C, 0x7E020418 }, // 2
-    { 0x3840423C, 0x3C424040 }, // 3
-    { 0x7E424478, 0x40404040 }, // 4
-    { 0x3E02027E, 0x3C424040 }, // 5
-    { 0x3E02423C, 0x3C424242 }, // 6
-    { 0x2040407E, 0x10101010 }, // 7
-    { 0x3C42423C, 0x3C424242 }, // 8
-    { 0x7C42423C, 0x3C424040 }, // 9
-    { 0x66663C18, 0x66667E7E }, // A
-    { 0x3E66663E, 0x3E666666 }, // B
-    { 0x0202423C, 0x3C420202 }, // C
-    { 0x4444443E, 0x3E444444 }, // D
-    { 0x3E02027E, 0x7E020202 }, // E
-    { 0x1E02027E, 0x02020202 }, // F
-    { 0x7E181800, 0x0018187E }, // +
-    { 0x00181800, 0x00181800 }  // :
-  };
-  int exit = 0, x = 0, y = 0, cnt = 0;
   mt_setbgcolor (GREY);
-  g_static ();
+  g_drawborders ();
+  int exit = 0, x = 0, y = 0, frame = 0;
   while (!exit)
     {
+      g_drawboxes ();
+      int count, flag;
+      sc_countGet (&count);
+      x = count / 10;
+      y = count % 10;
+      g_highlightmemory (x, y);
+      g_drawboxes ();
       enum keys key = KEY_DEFAULT;
-      g_init (big);
-      g_memorybox ();
-      g_printmem (&x, &y);
-      g_counterbox ();
-      g_bcbox (big);
       rk_readkey (&key);
       switch (key)
         {
-        case KEY_ESCAPE:
+        case KEY_Q:
           exit++;
           break;
         case KEY_UP:
@@ -401,22 +487,27 @@ g_interface ()
           g_savememory ();
           break;
         case KEY_I:
-          sc_memoryInit ();
+          raise (SIGUSR1);
           x = 0, y = 0;
-          g_static ();
+          g_drawboxes ();
           break;
         case KEY_ENTER:
           g_setmemory (x, y);
           break;
+        case KEY_R:
+          sc_regGet (FLAG_IGNORE, &flag);
+          sc_regSet (FLAG_IGNORE, !flag);
+          alarm (flag);
+          break;
         default:
           break;
         }
-      if (cnt == 5)
+      if (frame == 5)
         {
-          g_static ();
-          cnt = 0;
+          g_drawborders ();
+          frame = 0;
         }
-      cnt++;
+      frame++;
     }
   return 0;
 }
