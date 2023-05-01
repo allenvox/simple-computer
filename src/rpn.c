279106ced74cbdd978efe0a4eb97c4f24d3cd3a5
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-05-01 10:54:57 +0700

    Comment code in RPN module

--- src/rpn.c
+++ src/rpn.c
@@ -63,19 +63,25 @@ char *
 translateToRPN (char *inf, char *rpn)
 {
   node *root = NULL;
-  int i = 0, j = 0;
-  while (inf[i] != '\0' && inf[i] != '\n')
+  int j = 0;
+  for (int i = 0; inf[i] != '\0' && inf[i] != '\n'; i++)
     {
       char x = inf[i];
+
+      // if current char is variable/constant - just add to RPN str
       if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
         {
           rpn[j] = x;
           j++;
         }
+
+      // if open parentheses - push to stack
       else if (x == '(')
         {
           stack_push (x, &root);
         }
+
+      // if closed parentheses - pop all operations till open parentheses
       else if (x == ')')
         {
           while (stack_top (root) != '(')
@@ -89,6 +95,8 @@ translateToRPN (char *inf, char *rpn)
             }
           stack_pop (&root);
         }
+
+      // if operator - check its priority and add 
       else if (x == '+' || x == '-' || x == '*' || x == '/')
         {
           while (root != NULL
@@ -103,12 +111,16 @@ translateToRPN (char *inf, char *rpn)
             }
           stack_push (x, &root);
         }
+
+      // if neither var/const/operator/space - wrong character
       else if (x != ' ')
         {
           errOutput ("Wrong expression\n");
         }
       i++;
     }
+
+  // pop all operations and add them to RPN str
   while (root != NULL)
     {
       char c = stack_pop (&root);
@@ -118,11 +130,13 @@ translateToRPN (char *inf, char *rpn)
           j++;
         }
     }
+
+  // if left any parentheses - user inputted too much of them
   for (int k = 0; k < j; k++)
     {
       if (rpn[k] == '(' || rpn[k] == ')')
         {
-          errOutput ("Check your expression!\n");
+          errOutput ("Check your expression for unneeded parentheses\n");
         }
     }
   rpn[j] = '\0';
